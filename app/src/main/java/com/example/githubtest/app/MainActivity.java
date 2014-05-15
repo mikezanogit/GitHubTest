7a3ff7a78543e7e590433715c59bc8a891bcb2c8
mikezano <mikezanogit@gmail.com>
2014-05-15 15:59:15 -0700

    Passing information between activities

--- app/src/main/java/com/example/githubtest/app/MainActivity.java
+++ app/src/main/java/com/example/githubtest/app/MainActivity.java
@@ -5,16 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+    private static final int SUBACTIVITY_REQUEST_CODE = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,8 +50,29 @@ public class MainActivity extends Activity {
     private void onShowSubActivity1()
     {
         Intent intent = new Intent(this,SubActivity1.class);
-        startActivity(intent);
+        startActivityForResult(intent, SUBACTIVITY_REQUEST_CODE);
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case SUBACTIVITY_REQUEST_CODE:
+                handleProvideInfoResult(resultCode, data);
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleProvideInfoResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            String name = data.getStringExtra(SubActivity1.NAME_EXTRA);
+            TextView main = (TextView) findViewById(R.id.textViewMain);
+            main.setText(name);
+        }
+        else {
+            Toast.makeText(this, "User Canceled", Toast.LENGTH_LONG).show();
+        }
+    }
 
 }
--- app/src/main/java/com/example/githubtest/app/SubActivity1.java
+++ app/src/main/java/com/example/githubtest/app/SubActivity1.java
@@ -1,18 +1,63 @@
 package com.example.githubtest.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class SubActivity1 extends ActionBarActivity {
+public class SubActivity1 extends Activity {
+
+
+
+    public static final String NAME_EXTRA = "nameExtra";
+
+    public EditText getmNameTextValue() {
+        if (mNameTextValue == null) {
+            mNameTextValue = (EditText) findViewById(R.id.editTextName);
+        }
+        return mNameTextValue;
+    }
+
+    private EditText mNameTextValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_activity1);
+
+        setupViews();
     }
+
+    private void setupViews() {
+        Button saveButton = (Button) findViewById(R.id.emoticonSaveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSaveButton((Button) view);
+            }
+        });
+
+    }
+
+    private void handleSaveButton(Button view) {
+
+        Editable editable = this.getmNameTextValue().getText();
+        String name = editable != null ? editable.toString(): "";
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(NAME_EXTRA, name);
+
+        setResult(RESULT_OK, resultIntent); //up to here all you do is load up data
+        finish(); //this actually makes it available to next activity
+    }
+
 
 
     @Override
